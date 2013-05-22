373e470421d2a45eac45b85aeeedbe1c6fa094c7
Martin Brochhaus <mbrochh@gmail.com>
2013-05-22 18:45:05 +0800

    Fixed bugs with person plugin format strings

--- multilingual_project_blog/formats/zh/formats.py
+++ multilingual_project_blog/formats/zh/formats.py
@@ -4,4 +4,4 @@ TIME_FORMAT = 'H:i'
 DATETIME_FORMAT = 'Y年m月d日, H:i'
 
 LONG_NAME_FORMAT = '{L}, {f} {g}'
-LONG_NAME_FORMAT_NON_ROMAN = '{a}{x}{g} ({L}, {f})'
+LONG_NAME_FORMAT_NON_ROMAN = '{x}{a}{g} ({L}, {f})'
