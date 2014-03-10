e03288694d46109650f7e01af190efcbf19ddbbc
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2014-03-10 16:41:25 +0100

    Simplified session_language context_processor.

--- multilingual_project_blog/context_processors.py
+++ multilingual_project_blog/context_processors.py
@@ -50,11 +50,8 @@ def session_language(request):
        available.
 
     """
-    if 'django_language' in request.session:
-        lang = request.session['django_language']
-    else:
-        lang = translation.get_language_from_request(request)
-        request.session['django_language'] = lang
     if 'lang' in request.GET:
         lang = request.GET.get('lang')
+    else:
+        lang = translation.get_language()
     return {'SESSION_LANGUAGE': lang, }
