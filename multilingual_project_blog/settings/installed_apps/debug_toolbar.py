a20e3c505cdc173c7d40cc41592f306af48f3bc4
Martin Brochhaus <mbrochh@gmail.com>
2014-09-24 19:02:16 +0800

    Moved debug config into local_settings / fixed placeholder bug

--- multilingual_project_blog/settings/installed_apps/debug_toolbar.py
+++ multilingual_project_blog/settings/installed_apps/debug_toolbar.py
@@ -1,6 +1,2 @@
 """Settings for the ``debug_toolbar`` app."""
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-    'SHOW_TOOLBAR_CALLBACK': 'lcfc.settings.local_settings.show_toolbar',
-}
